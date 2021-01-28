6291ffba65bd50ca754c50ebd1fe014f7cf68eb8
blakefaris <blakefaris@gmail.com>
2021-01-28 14:49:35 -0600

    Initial commit from Spring Initializr

---
+++ src/main/java/com/blakefaris/slow/SlowApplication.java
@@ -0,0 +1,13 @@
+package com.blakefaris.slow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowApplication.class, args);
+	}
+
+}
